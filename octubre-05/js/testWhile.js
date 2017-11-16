9ac00cacae31c42b49a85d1a5d2e7d3c7ac82be5
D4n1Ela <adaDana@correoFalso.com>
2017-11-15 21:29:44 -0300

    Javascript: While.

---
+++ octubre-05/js/testWhile.js
@@ -0,0 +1,49 @@
+/********************* EJERCICIO A *********************/
+/*var x = 1;
+while ( x<= 3){
+	console.log(x);
+	x++;
+}
+console.log("Sali y el valor de X es:" + x);
+console.log(`Sali y el valor de X es ${x}`);*/
+
+
+
+/********************* EJERCICIO B *********************/
+/*var x = 200;
+while ( x != 0 && x <= 100){
+	console.log(x);
+	x--;
+}
+console.log("Sali y el valor de X es:" + x);
+console.log(`Sali y el valor de X es ${x}`);
+
+*/
+
+/********************* EJERCICIO C *********************/
+var x = 1;
+while ( x <= 100){
+	if(x%2 == 0){
+    	console.log(x);    
+	}
+	
+	x++;
+}
+console.log("Sali y el valor de X es:" + x);
+console.log(`Sali y el valor de X es ${x}`);
+
+/********************* EJERCICIO D *********************/
+var x = 100;
+while ( x >= 1){
+	if(x%2 == 0){
+    	console.log(x);    
+	}
+	
+	x--;
+}
+console.log("Sali y el valor de X es:" + x);
+console.log(`Sali y el valor de X es ${x}`);
+
+
+/********************* EJERCICIO E *********************/
+DATO: X+= 2
