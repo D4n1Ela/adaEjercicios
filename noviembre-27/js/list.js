51f372db2e153df48b35cf6464f65a993aff2961
D4n1Ela <adaDana@correoFalso.com>
2017-11-28 10:22:10 -0300

    Agrego la opción de que el código que borra el li este fuera de la función Add.

--- noviembre-27/js/list.js
+++ noviembre-27/js/list.js
@@ -2,20 +2,35 @@
 
 $(document).ready(function(){
 
-$("#myButton").on("click", function(){
-	/*
-	var myContent = $("#myInput").val();
-	var myLi ='<li>. '+myContent+'<button class="btnDelete" type="button">x</button></li>';
-	if ($("#myCheckbox").is(":checked")) {
-			//$("li").addClass("myRed");
-			$(myLi).css("color","red");
-		}
-	//$("#myCheckbox").prop('checked',false);	
-	$("#myUl").append(myLi);
-	$("#myInput").val("");
+	$("#myButton").on("click", function(){
+		
+		var myContent = $("#myInput").val();
+		var myLi ='<li>. '+myContent+'<button class="btnDelete" type="button">x</button></li>';
+		$("#myUl").append(myLi);
+		$("#myInput").val("");
+		if ($("#myCheckbox").is(":checked")) {
+				$("li").last().addClass("myRed");
+				//$(myLi).css("color","red");
+			}
+		$("#myCheckbox").prop('checked',false);	
+		/*
+		$(".btnDelete").on("click", function(){
+			$(this).parent().remove();
+		})	
+	    */
+	});
 
-*/
+	function deleteFather(){
+		$(this).parent().remove();
+	}
+ 
+    $(document).on("click", ".btnDelete", deleteFather);
+
+
+
 	
+})
+/*	
    var myArray = [];
    myArray.push($("#myInput").val());
 
@@ -35,10 +50,8 @@ $("#myButton").on("click", function(){
 	$(".btnDelete").on("click", function(){
 		$(this).parent().remove();
 	})
-
-});
-
-})	
+*/
+	
 
 
 
@@ -48,4 +61,18 @@ $("#myButton").on("click", function(){
 $(".btnDelete").on("click", function(){
 	$(this).parent();
 })
+*/
+
+/*
+
+docuemnt ready tengo todas las variable cargadas.
+
+Como la clase .btnDelete esta dentro de los li que son variables locales en las 
+la funcion principal 
+Si lo siguiente no esta dentro 
+$(".btnDelete").on("click", function(){
+		$(this).parent().remove();
+	})	
+
+Funcion anonima si lo que se hace se hace adentro
 */
